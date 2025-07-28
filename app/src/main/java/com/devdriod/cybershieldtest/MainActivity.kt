86bb82fd06d57ab7aa337f6f5c17ba7b80e6b353
Muhammad Awais <137446210+muhammad7865@users.noreply.github.com>
2025-07-28 17:25:19 +0500

    First

---
+++ app/src/main/java/com/devdriod/cybershieldtest/MainActivity.kt
@@ -0,0 +1,62 @@
+// MainActivity.kt
+package com.devdriod.cybershieldtest
+
+import android.R.style
+import android.graphics.Paint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.WhitePoint
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF1E1E2F)), // Background color
+                contentAlignment = Alignment.Center // Centers the Column inside the Box
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Welcome to the",
+                        color = Color.White
+                    )
+                    Text(
+                        text = "Cyber Shield app",
+                        color = Color.Cyan
+                    )
+                }
+            }
+        }
+    }
+}
+
+
